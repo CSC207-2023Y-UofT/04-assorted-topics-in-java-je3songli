3d9223e6bc3ced5ef459cdc36ebcc0af117f6425
je3songli <58784080+je3songli@users.noreply.github.com>
2023-05-28 10:01:37 -0400

    Completed tasks on DrivableTrader.java

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -11,3 +11,44 @@
  */
 
 import java.util.List;
+class DrivableTrader extends Trader<Drivable> {
+    /**
+     * Construct a DrivableTrader, giving them the
+     * given inventory, wishlist, and money.
+     *
+     * @param inventory Objects in this Trader's inventory
+     * @param wishlist  Objects in this Trader's wishlist
+     * @param money     The Trader's money
+     */
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist,
+                          int money) {
+        super(inventory, wishlist, money);
+    }
+
+    /**
+     * Construct a DrivableTrader, giving them the
+     * given money and an empty wishlist and inventory.
+     *
+     * @param money     The Trader's money
+     */
+    public DrivableTrader(int money) {
+        super(money);
+    }
+
+    /**
+     * Takes in a Drivable object and returns its price + max speed when tradable.
+     * When not tradable, returns Tradable.MISSING_PRICE.
+     * @param item The item to get a price for
+     */
+    @Override
+    public int getSellingPrice(Drivable item) {
+        int super_price = super.getSellingPrice(item);
+
+        if (super_price == Tradable.MISSING_PRICE) {
+            return super_price;
+        }
+
+        return super_price + item.getMaxSpeed();
+
+    }
+}
